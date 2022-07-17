d6e993e4c517cd52966e0cb596e445f00bd0298d
SomiyaKumari23 <93765781+SomiyaKumari23@users.noreply.github.com>
2022-07-17 16:33:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,26 +14,26 @@ document.addEventListener("keypress",function(event){
 function makesound(key){
     switch (key) {
         case "w":
-            var snd = new Audio("sounds/tom-1.mp3"); // buffers automatically when created
+            var snd = new Audio("tom-1.mp3"); // buffers automatically when created
             snd.play();
             break;
         case "a":
-             var snd1 = new Audio("sounds/tom-2.mp3"); // buffers automatically when created
+             var snd1 = new Audio("tom-2.mp3"); // buffers automatically when created
             snd1.play();
         case "s":
-            var snd2 = new Audio("sounds/tom-3.mp3"); // buffers automatically when created
+            var snd2 = new Audio("tom-3.mp3"); // buffers automatically when created
             snd2.play();
         case "d":
-           var snd3 = new Audio("sounds/tom-4.mp3"); // buffers automatically when created
+           var snd3 = new Audio("tom-4.mp3"); // buffers automatically when created
             snd3.play();
         case "j":
-            var snd4 = new Audio("sounds/crash.mp3"); // buffers automatically when created
+            var snd4 = new Audio("crash.mp3"); // buffers automatically when created
             snd4.play();
         case "k":
-            var snd5 = new Audio("sounds/kick-bass.mp3"); // buffers automatically when created
+            var snd5 = new Audio("kick-bass.mp3"); // buffers automatically when created
             snd5.play();
         default:
-           var snd6 = new Audio("sounds/snare.mp3"); // buffers automatically when created
+           var snd6 = new Audio("snare.mp3"); // buffers automatically when created
             snd6.play();
             break;
        }
